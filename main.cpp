f20ba87f39c35a1b86edaf9df0cd908472c7f1eb
rana <fusionbomb2004@gmail.com>
2025-03-07 01:01:12 +0530

    add pawn attack generation

--- main.cpp
+++ main.cpp
@@ -15,6 +15,10 @@ enum{
 
 };
 
+enum  {
+	white, black	
+};
+
 /*"a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
 "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
 "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
@@ -25,44 +29,97 @@ enum{
 "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",*/
 
 
-U64 set_bit(U64 bitboard, int square_no) {
-  return (bitboard | (1ULL << square_no));
+void set_bit(U64 &bitboard, int square_no) {
+  	bitboard = bitboard | (1ULL << square_no);
 }
 
-U64 clear_bit(U64 bitboard, int square_no) {
-  return (bitboard & (~(1ULL << square_no)));
+void clear_bit(U64 &bitboard, int square_no) {
+  	bitboard = (bitboard & (~(1ULL << square_no)));
 }
 
-int get_bit(U64 bitboard, int square_no) { return (bitboard >> square_no) & 1; }
+int get_bit(const U64 &bitboard, int square_no) { return (bitboard >> square_no) & 1; }
 
-U64 flip_bit(U64 bitboard, int square_no) {
-  if (get_bit(bitboard, square_no) == 1) {
-    return (clear_bit(bitboard, square_no));
-  }
-  return (set_bit(bitboard, square_no));
+void flip_bit(U64 &bitboard, int square_no) {
+  	if (get_bit(bitboard, square_no) == 1) {
+    	clear_bit(bitboard, square_no);
+  	} else {
+  		set_bit(bitboard, square_no);
+	}
 }
 
 void print_bitboard(U64 bitboard) {
-  int square = 0;
-  for (int rank = 0; rank < 8; rank++) {
-    for (int file = 0; file < 8; file++) {
-
-      if (!file) {
-        std::cout << " " << 8 - rank << "  ";
+  	int square = 0;
+  	for (square = 0; square < 64; square++) {
+		if(square % 8 == 0){
+			std::cout<<"\n";
+		}
+		
+		if (!(square % 8)) {
+        	std::cout << " " << 8 - square/8 << "  ";
       }
-      square = rank * 8 + file;
-      std::cout << " " << get_bit(bitboard, square);
-    }
+      
+		std::cout << " " << get_bit(bitboard, square);
+   }
 
-    std::cout << "\n";
-  }
-  std::cout << "\n     a b c d e f g h";
-  std::cout << "\n   bitboard value = " << bitboard << "\n\n";
+   std::cout << "\n";
+  	
+	std::cout << "\n     a b c d e f g h";
+  	std::cout << "\n   bitboard value = " << bitboard << "\n\n";
 }
 
+
+//attacks
+U64 pawn_attacks[2][64];
+
+U64 pawn_white_attack_gen(int position){
+	U64 attack = 0ULL;
+	if(position % 8 == 0){
+		set_bit(attack, position - 7);
+		return attack;
+	}
+
+	if(position % 8 == 7){
+		set_bit(attack, position - 9);
+		return attack;
+	}
+
+	set_bit(attack, position - 7);
+	set_bit(attack, position - 9);
+	return attack;
+}
+
+U64 pawn_black_attack_gen(int position){
+	U64 attack = 0ULL;
+	if(position % 8 == 0){
+		set_bit(attack, position + 9);
+		return attack;
+	}
+
+	if(position % 8 == 7){
+		set_bit(attack, position + 7);
+		return attack;
+	}
+
+	set_bit(attack, position + 7);
+	set_bit(attack, position + 9);
+	return attack;
+}
+U64 pawn_attacks_gen(int side, int position){
+	if(side == black){
+		return pawn_black_attack_gen(position);
+	}
+
+	return pawn_white_attack_gen(position);
+
+}
 int main() {
-	U64 board = 0ULL;
-	board = set_bit(board,d5);
-	print_bitboard(board);
+	U64 test = 0ULL;
+	set_bit(test, a3 + 1);
+	//generate pawn moves
+	for(int i = 8; i < 56; i++){
+		pawn_attacks[white][i] = pawn_attacks_gen(white,i);
+		pawn_attacks[black][i] = pawn_attacks_gen(black,i);
+	}
+	print_bitboard(pawn_attacks[black][h2]);
 	return 0;
 }
